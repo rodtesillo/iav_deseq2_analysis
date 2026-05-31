95e0f5d6182cc24eb7418fd971dd665bb38d921c
rodtesillo <rodrigo_tesillo@icloud.com>
2026-05-31 14:56:27 -0600

    feat: Crear función para clasificar los niveles de expresión

--- analyze_iav.py
+++ analyze_iav.py
@@ -11,3 +11,15 @@ def is_significant(log2_fold_change, padj, lfc_threshold, padj_threshold):
         return True
 
     return False
+
+
+# Crear función para clasificar genes en upregulated y downregulated
+def classify_gene(log2_fold_change):
+    """Evalúa si un gen aumenta o disminuye su expresión."""
+
+    if log2_fold_change > 0:
+        return "upregulated"
+    elif log2_fold_change < 0:
+        return "downregulated"
+    else:
+        return "Sin cambios"
